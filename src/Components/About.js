3728db99360cd1f6032e754311746b57052f7769
Phan Kỳ <kypdit92@gmail.com>
2021-06-02 16:36:47 +0700

    doneRESUME

---
+++ src/Components/About.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const About = ({ data }) => {
+  if (data) {
+    var name = data.name;
+    var profilepic = "images/" + data.image;
+    var bio = data.bio;
+    var street = data.address.street;
+    var state = data.address.state;
+    var city = data.address.city;
+    var phone = data.phone;
+    var email = data.email;
+  }
+
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img className="profile-pic" src={profilepic} alt="Profile Pic" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <span>
+                  {street}
+                  <br />
+                  {state}, {city}
+                </span>
+                <br />
+                <span>{phone}</span>
+                <br />
+                <span>{email}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
